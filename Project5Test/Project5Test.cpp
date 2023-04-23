0ac4da26a0a328dff54141872a1a1b5f9ec0ea77
newmanpj21 <43393873+newmanpj21@users.noreply.github.com>
2023-04-22 20:22:28 -0400

    Add Visual Studio Unit testing

---
+++ Project5Test/Project5Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Project5Test
+{
+	TEST_CLASS(Project5Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
